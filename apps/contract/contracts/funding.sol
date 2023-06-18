e84eb43ced8924f475d1e35c0bb4e1ba649393e1
DkDeepak001 <Deepakeswar001@gmail.com>
2023-06-18 20:25:04 +0530

    feat: :fire: add accept token in funding contract

--- apps/contract/contracts/funding.sol
+++ apps/contract/contracts/funding.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Funding {
 
     struct Campaign {
@@ -19,10 +20,27 @@ contract Funding {
 
     mapping (uint256 => Campaign) campaigns;
 
+    IERC20 public immutable tokenAddress;
+
+    constructor (IERC20 _tokenAddress){
+        tokenAddress =_tokenAddress;
+    }
+
     uint public campaignCount;
+    //TODO get all campaing 
+    //TODO make this pay using specfing token
+    
 
     function createCampaing(string memory _title,string memory  _descripition,uint _target,string memory image)  external {
         Campaign storage campaign = campaigns[campaignCount];
+        
+        require(bytes(_title).length > 0, "Title must not be empty");
+        require(bytes(_descripition).length > 0, "Description must not be empty");
+        require(_target > 0, "Target must be greater than 0");
+        require(bytes(image).length > 0, "Image must not be empty");
+        require(msg.sender != address(0), "Owner must be a valid address");
+
+        
 
         campaign.owner = msg.sender;
         campaign.title = _title;
@@ -38,16 +56,19 @@ contract Funding {
         }
 
 
-        function donate(uint campaingId) external payable {
-            require(msg.value >= 0,"you need to donate atleast 1 token");
-            require(campaigns[campaingId].deadline >= block.timestamp,"Campaign has been closed");
+    function donate(uint campaignId, uint amount) external {
+        require(amount > 0, "Donation amount must be greater than 0");
+        require(campaigns[campaignId].deadline >= block.timestamp, "Campaign has been closed");
 
-            Campaign storage campaign = campaigns[campaingId];
+        // Transfer tokens from the donor to the contract
+        require(tokenAddress.transferFrom(msg.sender, address(this), amount), "Token transfer failed");
 
-            campaign.recievedAmount += msg.value;
-            campaign.donorAddresses.push(msg.sender);
-            campaign.donationAmounts.push(msg.value);
-        }
+        Campaign storage campaign = campaigns[campaignId];
+
+        campaign.recievedAmount += amount;
+        campaign.donorAddresses.push(msg.sender);
+        campaign.donationAmounts.push(amount);
+    }
 
 
 }
